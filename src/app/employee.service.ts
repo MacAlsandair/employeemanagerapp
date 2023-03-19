def28acc8a8298e464c5a6c88fcf0908696c16d2
MacAlsandair <mac.alsandair@gmail.com>
2023-03-20 00:14:54 +0200

    Add enviroment variable apiBaseUrl

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Employee } from "./employee";
 import { HttpClient } from "@angular/common/http";
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor (private http: HttpClient) {}
 
