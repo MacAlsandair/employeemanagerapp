2efa257f33755c47e7a256edac4a2b1e3e5bd33b
MacAlsandair <mac.alsandair@gmail.com>
2023-03-23 23:01:15 +0200

    Test on jenkins-github compatibility

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -21,7 +21,11 @@ export class EmployeeService {
     return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
   }
 
-  public updateEmployee(employeeId: number): Observable<void> {
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
 }
