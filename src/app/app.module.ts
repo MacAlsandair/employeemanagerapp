959dd1e627050e197ebbff42639768ea97bce4a2
MacAlsandair <mac.alsandair@gmail.com>
2023-03-25 01:11:56 +0200

    test on jenkins-github compatibility 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,4 +16,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-//test on jenkins-github compatibility 3
+//test on jenkins-github compatibility 4
